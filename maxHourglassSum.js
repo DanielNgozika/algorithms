e439d49f9f44c44e893963f23bb79c96eee218f9
Daniel Ngozika <danielngozikac@gmail.com>
2020-11-02 10:16:39 +0100

    Solve max hour glass sum.

---
+++ maxHourglassSum.js
@@ -0,0 +1,30 @@
+function hourglassSum(arr) {
+	let hourglassSums = [];
+	for (let i = 0; i < arr.length - 2; i++) {
+		for (let j = 0; j < arr[i].length - 2; j++) {
+			hourglassSums.push(
+				arr[i][j] +
+					arr[i][j + 1] +
+					arr[i][j + 2] +
+					arr[i + 1][j + 1] +
+					arr[i + 2][j] +
+					arr[i + 2][j + 1] +
+					arr[i + 2][j + 2]
+			);
+		}
+	}
+
+	return Math.max(...hourglassSums);
+}
+
+console.log(
+	"Output:",
+	hourglassSum([
+		[1, 1, 1, 0, 0, 0],
+		[0, 1, 0, 0, 0, 0],
+		[1, 1, 1, 0, 0, 0],
+		[0, 0, 2, 4, 4, 0],
+		[0, 0, 0, 2, 0, 0],
+		[0, 0, 1, 2, 4, 0]
+	])
+);
